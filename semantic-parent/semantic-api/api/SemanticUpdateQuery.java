97c55d9e6502cda2c769678062b9974ae16f6346
svetlozar321@gmail.com <Svetlozar@DESKTOP-4S2QVVR>
2016-12-18 15:03:30 +0200

    Remove api folder

--- semantic-parent/semantic-api/api/SemanticUpdateQuery.java
+++
@@ -1,8 +0,0 @@
-package com.ontotext.semantic.core.api;
-
-import org.openrdf.repository.RepositoryConnection;
-
-public interface SemanticUpdateQuery extends SemanticQuery {
-
-	public void evaluate(RepositoryConnection connection);
-}
