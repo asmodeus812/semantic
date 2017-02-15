9bcc3a9e400eb3b1d41df9c454bd8d371fe23ae8
svetlozar321@gmail.com <svetlozar321@gmail.com>
2017-02-15 21:33:31 +0200

    Move the instance JSON parse specific helper methods to the JSON parser

--- semantic-parent/semantic-impl/src/main/java/com/ontotext/semantic/impl/parser/SemanticInstanceParser.java
+++ semantic-parent/semantic-impl/src/main/java/com/ontotext/semantic/impl/parser/SemanticInstanceParser.java
@@ -1,14 +1,25 @@
 package com.ontotext.semantic.impl.parser;
 
-import static com.ontotext.semantic.core.common.SemanticInstanceUtil.parseToString;
+import static com.ontotext.semantic.core.common.SemanticInstanceUtil.isValueInstance;
+import static com.ontotext.semantic.core.common.SemanticSparqlUtil.COLLON;
 import static com.ontotext.semantic.core.common.SemanticSparqlUtil.COMMA;
+import static com.ontotext.semantic.core.common.SemanticSparqlUtil.CURLY_BRACE_CLOSE;
+import static com.ontotext.semantic.core.common.SemanticSparqlUtil.CURLY_BRACE_OPEN;
+import static com.ontotext.semantic.core.common.SemanticSparqlUtil.ESCAPED_QUOTE;
+import static com.ontotext.semantic.core.common.SemanticSparqlUtil.SINGLE_SPACE;
 import static com.ontotext.semantic.core.common.SemanticSparqlUtil.SQUARE_BRACE_CLOSE;
 import static com.ontotext.semantic.core.common.SemanticSparqlUtil.SQUARE_BRACE_OPEN;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+
+import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.vocabulary.XMLSchema;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -16,6 +27,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.ontotext.semantic.api.enumeration.SemanticInstanceType;
 import com.ontotext.semantic.api.instance.Instance;
 import com.ontotext.semantic.api.parser.InstanceParser;
 import com.ontotext.semantic.impl.instance.SemanticInstance;
@@ -121,6 +133,118 @@ public class SemanticInstanceParser implements InstanceParser {
 		} else {
 			return Arrays.asList(new SemanticLiteral(value.getAsString()));
 		}
+	}
+
+	/**
+	 * Parses a given instance as a complete JSON formated string
+	 * 
+	 * @param instance
+	 *            the instance to be parsed
+	 * @param builder
+	 *            the builder where the formated JSON string will be stored
+	 */
+	public static void parseToString(Instance instance, StringBuilder builder) {
+		// Append JSON object beginning brace
+		builder.append(CURLY_BRACE_OPEN);
+
+		// Append the instance value wrapped around quotes
+		builder.append(wrapInQuotes(instance));
+		if (instance.getPropertyMap().size() != 0) {
+			boolean hasPropValues = hasPropertyValues(instance);
+			// Append begin block according to the instance properties state
+			appendBeginBlock(builder, hasPropValues);
+			// Append the parsed instance properties
+			parseProperties(instance, builder);
+			// Append finish block according to the instance properties state
+			appendFinishBlock(builder, hasPropValues);
+		} else {
+			// Append as empty JSON object
+			builder.append(COLLON);
+			builder.append(CURLY_BRACE_OPEN);
+			builder.append(CURLY_BRACE_CLOSE);
+		}
+		// Append JSON object finishing brace
+		builder.append(CURLY_BRACE_CLOSE);
+	}
+
+	private static void parseProperties(Instance instance, StringBuilder builder) {
+		int currentProperty = 0;
+
+		for (Map.Entry<Instance, ArrayList<Instance>> pair : instance.getPropertyMap().entrySet()) {
+			int currentInstance = 0;
+
+			++currentProperty;
+			builder.append(wrapInQuotes(pair.getKey()));
+
+			if (pair.getValue().size() != 0) {
+				builder.append(COLLON).append(SINGLE_SPACE);
+				builder.append(SQUARE_BRACE_OPEN);
+				for (Instance value : pair.getValue()) {
+					++currentInstance;
+					if (value.getInstanceType() == SemanticInstanceType.INSTANCE) {
+						parseToString(value, builder);
+					} else {
+						builder.append(wrapInQuotes(value));
+					}
+
+					if (currentInstance < pair.getValue().size()) {
+						builder.append(COMMA);
+					}
+				}
+				builder.append(SQUARE_BRACE_CLOSE);
+			}
+
+			if (currentProperty < instance.getPropertyMap().size()) {
+				builder.append(COMMA);
+			}
+			builder.append(System.lineSeparator());
+		}
+	}
+
+	private static void appendBeginBlock(StringBuilder builder, boolean hasPropertyValues) {
+		builder.append(SINGLE_SPACE).append(COLLON);
+		if (hasPropertyValues) {
+			builder.append(CURLY_BRACE_OPEN);
+		} else {
+			builder.append(SQUARE_BRACE_OPEN);
+		}
+		builder.append(System.lineSeparator());
+	}
+
+	private static void appendFinishBlock(StringBuilder builder, boolean hasPropertyValues) {
+		if (hasPropertyValues) {
+			builder.append(CURLY_BRACE_CLOSE);
+		} else {
+			builder.append(SQUARE_BRACE_CLOSE);
+		}
+		builder.append(SINGLE_SPACE);
+	}
+
+	private static String wrapInQuotes(Instance instance) {
+		Value value = instance.getInstanceValue();
+		boolean shouldWrap = isValueInstance(value);
+
+		if (value instanceof Literal) {
+			Literal literal = (Literal) value;
+			URI type = literal.getDatatype();
+			if (type.equals(XMLSchema.STRING)) {
+				shouldWrap = true;
+			}
+		}
+
+		if (shouldWrap) {
+			return ESCAPED_QUOTE + instance + ESCAPED_QUOTE;
+		}
+		return instance.toString();
+	}
+
+	private static boolean hasPropertyValues(Instance instance) {
+		for (Map.Entry<Instance, ArrayList<Instance>> pair : instance.getPropertyMap().entrySet()) {
+			if (pair.getValue().size() != 0) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
